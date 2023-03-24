3a1b2892f87bdce53aa382976d0db79855ecf34b
Kimono きもの <34915099+kimono-k@users.noreply.github.com>
2023-03-24 20:24:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Decision Tree
+
+- Een algoritme die een hierarchische boomstructuur maakt om te bepalen wat het belangrijkst is in een dataset.
+
+# Classificcation
+
+- Array data een label geven bijvoorbeeld "survived" of "died"
+
+# Tree visualiser
+
+- Daarmee kan een tree getekend mee worden.
+
+# Papa parse
+
+- Een library om CSV bestanden in te laden
---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * TODO: Voorspel of iemand zijn vakantie op de titanic gaat overleven
+ */
+
+import { DecisionTree } from "./libraries/decisiontree.js";
+import { VegaTree } from "./libraries/vegatree.js";
+
+const csvFile = "./data/titanic.csv"; // dataset
+const trainingLabel = "Survived"; // x.csv kolomnaam = gender
+const ignoredColumns = ["SibSp"]; // genegeerde kolom
+
+// Gebruik Papi Parse om 1 van de datasets te laden
+function loadData() {
+  Papa.parse(csvFile, {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => trainModel(results.data), // train het model met deze data
+  });
+}
+
+//
+// MACHINE LEARNING - Bouw de Decision Tree
+//
+function trainModel(data) {
+  let decisionTree = new DecisionTree({
+    ignoredAttributes: ignoredColumns,
+    trainingSet: data,
+    categoryAttr: trainingLabel,
+  });
+  classifier(decisionTree);
+}
+
+function classifier(decisionTree) {
+  // Classificeren van de data
+  let person = {
+    Name: "Cumings, Mrs. John Bradley",
+    Sex: "female",
+  };
+  predictor(decisionTree, person);
+}
+
+function predictor(decisionTree, person) {
+  let prediction = decisionTree.predict(person);
+  console.log(
+    `${person.Name} is gonna ${prediction === "1" ? "survive" : "die"}`
+  );
+  console.log("Fuck, my ankle!");
+  visualizer(decisionTree);
+}
+
+function visualizer(decisionTree) {
+  let json = decisionTree.toJSON();
+  new VegaTree("#view", 2300, 1000, json);
+}
+
+loadData();
